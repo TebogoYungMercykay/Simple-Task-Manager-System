a2b653a5d4b728398449e758640d805ea6a09a98
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2024-10-28 14:08:55 +0200

    (src) Added Function Templates for The Task Class

--- src/task.cpp
+++ src/task.cpp
@@ -2,6 +2,64 @@
 #define TASK_CPP
 #include "task.h"
 
-//Your code here
+template <class T>
+Task<T>::Task(int priority, std::string description, bool repeat, T data) {
+
+}
+
+template <class T>
+Task<T>::Task(const Task<T>& toCopy) {
+
+}
+
+template <class T>
+Task<T>::~Task() {
+
+}
+
+template <class T>
+int Task<T>::getPriority() {
+    return 0;
+}
+
+template <class T>
+std::string Task<T>::getDescription() {
+    return "";
+}
+
+template <class T>
+bool Task<T>::getRepeat() {
+    return false;
+}
+
+template <class T>
+T Task<T>::getData() {
+    return new T();
+}
+
+template <class T>
+Task<T>* Task<T>::getNext() {
+    return NULL;
+}
+
+template <class T>
+Task<T>* Task<T>::getPrev() {
+    return NULL;
+}
+
+template <class T>
+bool Task<T>::setNext(Task<T>* nextTask) {
+    return false;
+}
+
+template <class T>
+bool Task<T>::setPrev(Task<T>* prevTask) {
+    return false;
+}
+
+template <class T>
+bool Task<T>::operator==(const Task<T>& other) {
+    return false;
+}
 
 #endif
